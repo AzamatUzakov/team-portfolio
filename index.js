aec1ad578413447cd3681d903c6f939411c8d84f
AzamatUzakov <azamatakmal053@gmail.com>
2024-02-21 17:50:13 +0500

    fixed

--- index.js
+++ index.js
@@ -1,142 +1,58 @@
-var PARTICLE_NUM = 500;
-var PARTICLE_BASE_RADIUS = 0.5;
-var FL = 500;
-var DEFAULT_SPEED = 2;
-var BOOST_SPEED = 300;
+var layerCount = 5;
+var starCount = 400;
+var maxTime = 30;
+var universe = document.getElementById("universe");
+var w = window;
+var d = document;
+var e = d.documentElement;
+var g = d.getElementsByTagName("body")[0];
+var width = w.innerWidth || e.clientWidth || g.clientWidth;
+var height = w.innerHeight || e.clientHeight || g.clientHeight;
+for (var i = 0; i < starCount; ++i) {
+  var ypos = Math.round(Math.random() * 2000);
+  var star = document.createElement("div");
+  var speed = 1000 * (Math.random() * maxTime + 1);
+  star.setAttribute("class", "star" + (3 - Math.floor(speed / 1000 / 8)));
+  star.style.backgroundColor = "white";
 
-var canvas;
-var canvasWidth, canvasHeight;
-var context;
-var centerX, centerY;
-var mouseX, mouseY;
-var speed = DEFAULT_SPEED;
-var targetSpeed = DEFAULT_SPEED;
-var particles = [];
-
-window.addEventListener('load', function () {
-    canvas = document.getElementById('c');
-
-    var resize = function () {
-        canvasWidth = canvas.width = window.innerWidth;
-        canvasHeight = canvas.height = window.innerHeight;
-        centerX = canvasWidth * 0.5;
-        centerY = canvasHeight * 0.5;
-        context = canvas.getContext('2d');
-        context.fillStyle = 'rgb(255, 255, 255)';
-    };
-
-    document.addEventListener('resize', resize);
-    resize();
-
-    mouseX = centerX;
-    mouseY = centerY;
-
-    for (var i = 0, p; i < PARTICLE_NUM; i++) {
-        particles[i] = randomizeParticle(new Particle());
-        particles[i].z -= 500 * Math.random();
+  universe.appendChild(star);
+  star.animate(
+    [
+      {
+        transform: "translate3d(" + width + "px, " + ypos + "px, 0)"
+      },
+      {
+        transform:
+          "translate3d(-" + Math.random() * 256 + "px, " + ypos + "px, 0)"
+      }
+    ],
+    {
+      delay: Math.random() * -speed,
+      duration: speed,
+      iterations: 1000
     }
-
-    document.addEventListener('mousemove', function (e) {
-        mouseX = e.clientX;
-        mouseY = e.clientY;
-    }, false);
-
-    document.addEventListener('mousedown', function (e) {
-        targetSpeed = BOOST_SPEED;
-    }, false);
-
-    document.addEventListener('mouseup', function (d) {
-        targetSpeed = DEFAULT_SPEED;
-    }, false);
-
-    setInterval(loop, 1000 / 60);
-}, false);
-
-function loop() {
-    context.save();
-    context.fillStyle = 'rgb(0, 0, 0)';
-    context.fillRect(0, 0, canvasWidth, canvasHeight);
-    context.restore();
-
-    speed += (targetSpeed - speed) * 0.01;
-
-    var p;
-    var cx, cy;
-    var rx, ry;
-    var f, x, y, r;
-    var pf, px, py, pr;
-    var a, a1, a2;
-
-    var halfPi = Math.PI * 0.5;
-    var atan2 = Math.atan2;
-    var cos = Math.cos;
-    var sin = Math.sin;
-
-    context.beginPath();
-    for (var i = 0; i < PARTICLE_NUM; i++) {
-        p = particles[i];
-
-        p.pastZ = p.z;
-        p.z -= speed;
-
-        if (p.z <= 0) {
-            randomizeParticle(p);
-            continue;
-        }
-
-        cx = centerX - (mouseX - centerX) * 1.25;
-        cy = centerY - (mouseY - centerY) * 1.25;
-
-        rx = p.x - cx;
-        ry = p.y - cy;
-
-        f = FL / p.z;
-        x = cx + rx * f;
-        y = cy + ry * f;
-        r = PARTICLE_BASE_RADIUS * f;
-
-        pf = FL / p.pastZ;
-        px = cx + rx * pf;
-        py = cy + ry * pf;
-        pr = PARTICLE_BASE_RADIUS * pf;
-
-        a = atan2(py - y, px - x);
-        a1 = a + halfPi;
-        a2 = a - halfPi;
-
-        context.moveTo(px + pr * cos(a1), py + pr * sin(a1));
-        context.arc(px, py, pr, a1, a2, true);
-        context.lineTo(x + r * cos(a2), y + r * sin(a2));
-        context.arc(x, y, r, a2, a1, true);
-        context.closePath();
-    }
-    context.fill();
+  );
 }
-
-function randomizeParticle(p) {
-    p.x = Math.random() * canvasWidth;
-    p.y = Math.random() * canvasHeight;
-    p.z = Math.random() * 1500 + 500;
-    return p;
-}
-
-
-/**
- * Particle
+/* 
+var elem = document.querySelector(".pulse");
+var animation = elem.animate(
+  {
+    opacity: [0.5, 1],
+    transform: ["scale(0.5)", "scale(1)"]
+  },
+  {
+    direction: "alternate",
+    duration: 500,
+    iterations: Infinity
+  }
+);
  */
-function Particle(x, y, z) {
-    this.x = x || 0;
-    this.y = y || 0;
-    this.z = z || 0;
-    this.pastZ = 0;
-}
-
-
 
 let menu = document.querySelector('.menu')
 let toggle = document.querySelector('.toggle')
 toggle.onclick = function() {
   menu.classList.toggle('active')
+  console.log("click");
 }   
 
 
