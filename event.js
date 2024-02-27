2219c841a0ca36a87b8a1c3cbcdc9ec956656507
AzamatUzakov <azamatakmal053@gmail.com>
2024-02-28 00:15:46 +0500

    fixed

--- event.js
+++ event.js
@@ -1,7 +1,43 @@
 let burgerMenu_btn = document.querySelector('.burger__menu')
 let Menu_contener = document.querySelector('.container')
+let swiper_box = document.querySelector(".mySwiper")
+let participant_box = document.querySelectorAll('.participant_box')
+
 
 burgerMenu_btn.onclick = () => {
-Menu_contener.classList.toggle("menu_block_active")
-console.log("click ");
+    Menu_contener.classList.toggle("menu_block_active")
+    console.log("click ");
 }
+
+
+participant_box.forEach(btn => {
+    btn.onmouseenter = () => {
+        participant_box.forEach(el => el.classList.remove("active"))
+        btn.classList.add("active")
+    }
+})
+/*  
+swiper_box.onmouseenter = () => {
+text_shadow.forEach(text => {
+   text.style.display = "block"
+
+});
+
+box_shadow.forEach(box => {
+    box.style.display = "block"
+    console.log("clicl");
+});
+
+}
+
+swiper_box.onmouseleave = () => {
+text_shadow.forEach(text => {
+    text.style.display = "none"
+ 
+ });
+ 
+ box_shadow.forEach(box => {
+     box.style.display = "none"
+     console.log("clicl");
+ });
+} */
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var layerCount = 5;
 var starCount = 400;
-var maxTime = 30;
+var maxTime = 34;
 var universe = document.getElementById("universe");
 var w = window;
 var d = document;
@@ -9,10 +9,10 @@ var g = d.getElementsByTagName("body")[0];
 var width = w.innerWidth || e.clientWidth || g.clientWidth;
 var height = w.innerHeight || e.clientHeight || g.clientHeight;
 for (var i = 0; i < starCount; ++i) {
-  var ypos = Math.round(Math.random() * 2000);
+  var ypos = Math.round(Math.random() * 4000);
   var star = document.createElement("div");
   var speed = 1000 * (Math.random() * maxTime + 1);
-  star.setAttribute("class", "star" + (3 - Math.floor(speed / 1000 / 8)));
+  star.setAttribute("class", "star" + (3 - Math.floor(speed / 10000 / 8)));
   star.style.backgroundColor = "white";
 
   universe.appendChild(star);
@@ -56,6 +56,19 @@ toggle.onclick = function() {
 }   
 
 
+/////////swiper///////////// */
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 10,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 
+});
 
 
